2aedaec673352c3cdf9ac7f19b2f500c488b0ab2
Adam Ulidowski <adam.ulidowski@wp.pl>
2016-11-09 11:14:01 +0100

    Tescik adama

--- app/src/androidTest/java/helpdesk/pl/helpdesk/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/helpdesk/pl/helpdesk/ExampleInstrumentedTest.java
@@ -12,6 +12,7 @@ import static org.junit.Assert.*;
 /**
  * Instrumentation test, which will execute on an Android device.
  *
+ *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
@@ -20,7 +21,7 @@ public class ExampleInstrumentedTest {
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
+        //Komentarz testowy adama
         assertEquals("helpdesk.pl.helpdesk", appContext.getPackageName());
     }
 }
